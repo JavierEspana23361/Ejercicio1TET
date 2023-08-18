0d03ac70f9b72309c2ff5b70371f6ac86d626817
JavierEspana23361 <esp23361@uvg.edu.gt>
2023-08-17 22:05:42 -0600

    V1

---
+++ DatosI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DatosI {
+    public void datosI() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Para iniciar el proceso de compra de tickets debe introducir los siguientes datos: ");
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese su email: ");
+        String email = scanner.nextLine();
+        System.out.println("Ingrese la cantidad de tickets que desea comprar (mínimo 1 ticket, máximo 6): ");
+        int ctickets = scanner.nextInt();
+        if(ctickets < 1 || ctickets > 6){
+            System.out.println("Esa no es una cantidad válida de tickets, ingrese una cantidad permitida (mínimo 1 ticket, máximo 6): ");
+            ctickets = scanner.nextInt();
+        }
+        System.out.println("Ingrese su presupuesto máximo en dólares: ");
+        int pmax = scanner.nextInt();
+    }
+}
---
+++ MainTET.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MainTET {
+    public static void main(String[] args) {
+        Random random = new Random();
+        DatosI DatosI = new DatosI();
+        System.out.println("¡Bienvenido/a al sistema de compra de tickets para The Eras Tour: by Tylor Swift!");
+        DatosI.datosI();
+    }
+}
