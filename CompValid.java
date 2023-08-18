874ec6f1a73f68259f40617836ba95bf813314d2
JavierEspana23361 <esp23361@uvg.edu.gt>
2023-08-18 06:54:00 -0600

    V4

--- CompValid.java
+++ CompValid.java
@@ -2,9 +2,9 @@ public class CompValid {
     public void validar(){
         RandomGC RandomGC = new RandomGC();
         RandomGC.rgenandcompare();
-
+        RandomLocal RandomLocal = new RandomLocal();   
         if(RandomGC.Apto() == true){
-            
+            RandomLocal.rlocalgen();
         }
         else{
             System.out.println("El sistema ha rechazado su compra.");
